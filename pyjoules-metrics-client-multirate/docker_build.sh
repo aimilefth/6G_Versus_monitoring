1f3b315243ca0d9eab71460701e1cbc700e45666
aimilefth <aimilefth@github.com>
2025-07-18 20:07:53 +0300

    Attempt to add multirate input using prometheus scraping, not working as intended

---
+++ pyjoules-metrics-client-multirate/docker_build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t aimilefth/pyjoules-metrics-client-multirate --push .
---
+++ pyjoules-metrics-client-multirate/docker_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run the pyJoules client container
+docker run \ --name pyjoules-metrics-client-multirate \
+  -d \
+  --network=host \
+  --privileged \
+  --pull=always \
+  aimilefth/pyjoules-metrics-client-multirate
+
+echo "pyJoules metrics client container started."
+echo "Metrics will be available at http://localhost:9092/metrics after a few seconds."
+echo "Note: The container is running in privileged mode to allow pyJoules access to MSR."
