4af625c66220eb99de7875acbb7c4293c8755d70
aimilefth <aimilefth@github.com>
2025-07-19 17:08:04 +0300

    Grafana implementation

---
+++ grafana/docker_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+GRAFANA_STORAGE_PATH="$(pwd)/grafana-storage"
+
+docker run --name grafana \
+  -d \
+  --network=host \
+  --user "$(id -u):$(id -g)"
+  -v "${GRAFANA_STORAGE_PATH}:/var/lib/grafana" \
+  --pull=always \
+  grafana/grafana:12.0.2
+
+echo "Grafana container started."
+echo "Access the Grafana UI at http://localhost:3000"
